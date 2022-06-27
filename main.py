c682975e65c9825634e8ca9f50ba7480f251f948
Manan Tyagi <63817084+MananTyagi@users.noreply.github.com>
2022-06-27 08:31:58 +0530

    adding comments

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def load_user(username: str):
    user = DB.get(username)
    return user
 
-
+#function for login, after valid and authenticated credentials, this function will set cookie to a access token
 @app.post("/auth/login")
 def login(data: OAuth2PasswordRequestForm = Depends()):
    username = data.username
@@ -41,19 +41,22 @@ def login(data: OAuth2PasswordRequestForm = Depends()):
    manager.set_cookie(resp, access_token)
    return resp
 
-
+#check point for authentication of user
 @app.get("/private")
 def getPrivateendpoint(_=Depends(manager)):
    return "You are an authentciated user"
 
 
 
-
+#for frontend
 @ app.get("/auth/login", response_class=HTMLResponse)
 def login(request: Request):
    context = {'request': request, }
    return templates.TemplateResponse("login.html.j2", context)
 
+
+
+#this function will lead the user to logout. for this we are settting up the cookie to empty string which is initially set to a access token (created by login manager)
 @app.get('/logout', response_class=HTMLResponse)
 def protected_route(request: Request, user=Depends(manager)):
     response = RedirectResponse(url="/login", status_code=status.HTTP_302_FOUND)
